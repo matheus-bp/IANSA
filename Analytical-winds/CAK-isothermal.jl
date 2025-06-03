6adf5337f312d831b174bb191804d3dd675bb4a9
matheus-bp <math.bp.sci@proton.me>
2025-06-03 22:21:31 +0200

    vcrit_ISOT_CAK

---
+++ Analytical-winds/CAK-isothermal.jl
@@ -0,0 +1,16 @@
+function v_crit(
+    r_crit    :: Float64,
+    Mstar     :: Float64, 
+    c_sound   :: Float64, 
+    alpha_CAK :: Float64, 
+    gamma_e   :: Float64; 
+    G_grav    :: Float64
+)
+"""
+    Computes the velocity at the critical point based on the conditions of an isothermal line-driven wind with non-negligible pressure term.
+"""
+
+    return ( a_sound^2 + (1/(1-alpha_CAK)) * ( (G_grav * Mstar * (1-gamma_e) / r_crit) - 2*c_sound^2 ) )^0.5
+
+end
+
