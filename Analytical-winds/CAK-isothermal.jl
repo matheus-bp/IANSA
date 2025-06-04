41bcd5be084290c2d6a75ef46705fd7b1586d718
matheus-bp <math.bp.sci@proton.me>
2025-06-04 10:33:25 +0200

    geometrical dillution

--- Analytical-winds/CAK-isothermal.jl
+++ Analytical-winds/CAK-isothermal.jl
@@ -1,3 +1,18 @@
+
+function W_geom(
+    Rstar :: Float64,
+    r     :: Float64
+)
+"""
+    Function that computes the geometrical dillution factor, which represents the solid angle covered by the star seen from the position "r"
+"""    
+    
+    return (1/2) * ( 1 - (1-(Rstar/r)^2)^0.5 )
+
+end
+
+########################
+
 function v_crit(
     r_crit    :: Float64,
     Mstar     :: Float64, 
