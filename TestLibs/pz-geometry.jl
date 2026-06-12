1789d58cef236417b998c1fd82228520cb246949
matheus-bp <math.bp.sci@proton.me>
2026-06-12 14:47:58 +0200

    plotting | wrong positioning of zaxis

--- TestLibs/pz-geometry.jl
+++ TestLibs/pz-geometry.jl
@@ -1,6 +1,6 @@
 # create a grid with the pz geometry scheme
 
-ND :: Int64 = 20 # number of depthpoints, i.e., the number of atmospheric impact parameters
+ND :: Int64 = 5 # number of depthpoints, i.e., the number of atmospheric impact parameters
 NC :: Int64 = 5 # number of core rays
 
 r_min :: Float64 = 1.0 # in stellar radii
@@ -25,11 +25,44 @@ zaxes_atm = [Vector{Float64}() for _ in 1:ND-1]
 zaxes_cor = [Vector{Float64}() for _ in 1:NC]
 
 for (j,p) in enumerate(imppar_atm)
-    for i in collect(Int64, 1:j)
+    for i in collect(Int64, j:-1:1)
         z = (radius[j]^2 - imppar_atm[i]^2)^0.5
         push!(zaxes_atm[j], z)
     end
 end
 
-println(zaxes_atm)
+########################
 
+if abspath(PROGRAM_FILE) == @__FILE__
+
+try
+    using CairoMakie
+catch
+    using Pkg
+    Pkg.add("CairoMakie")
+    using CairoMakie
+end
+const mak = CairoMakie  # Now mak is defined in global scope
+    
+fig = mak.Figure(size = (650,650))
+ax = mak.Axis(fig[1,1])
+
+for p in collect(1:ND-1)
+    imppar = fill(imppar_atm[p], length(zaxes_atm[p]))
+    zaxis = zaxes_atm[p]
+    println(imppar)
+    mak.scatter!(ax, zaxis, imppar[end:-1:1])
+end
+
+for r in radius
+    mak.arc!(ax, 0.0, r, 0, π/2, color=:blue, linewidth=0.5)
+end
+
+display(fig)
+println("Press Enter to close...")
+readline()  # Wait for user to press Enter
+
+########################
+else
+    println("imported pz-geometry.jl")
+end
