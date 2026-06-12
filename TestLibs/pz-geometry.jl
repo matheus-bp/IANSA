ce30e2a7a29bdd1daef7473821150be905839862
matheus-bp <math.bp.sci@proton.me>
2026-06-12 19:02:06 +0200

    Mirrored zrays

--- TestLibs/pz-geometry.jl
+++ TestLibs/pz-geometry.jl
@@ -1,6 +1,6 @@
 # create a grid with the pz geometry scheme
 
-ND :: Int64 = 20 # number of depthpoints, i.e., the number of atmospheric impact parameters
+ND :: Int64 = 50 # number of depthpoints, i.e., the number of atmospheric impact parameters
 NC :: Int64 = 7 # number of core rays
 
 r_min :: Float64 = 1.0 # in stellar radii
@@ -18,6 +18,7 @@ imppar_atm = radius[1:end-1]
 imppar_cor = collect(range(r_min, 0, length=NC)) # assumes a linear spacing of the core rays
 
 zrays_atm = [Vector{Float64}() for _ in 1:ND-1]
+zrays_aba =  [Vector{Float64}() for _ in 1:ND-1]
 zrays_cor = [Vector{Float64}() for _ in 1:NC]
 
 for (j,p) in enumerate(imppar_atm)
@@ -25,6 +26,17 @@ for (j,p) in enumerate(imppar_atm)
         z = (radius[i]^2 - p^2)^0.5
         push!(zrays_atm[j], z)
     end
+end
+
+for (j,p) in enumerate(imppar_atm)
+    for i in collect(Int64, 1:j)
+        z = -(radius[i]^2 - p^2)^0.5
+        push!(zrays_aba[j], z)
+    end
+end
+
+for z in zrays_aba
+    pop!(z)
 end
 
 for (j,p) in enumerate(imppar_cor)
@@ -47,18 +59,19 @@ catch
 end
 const mak = CairoMakie  
     
-fig = mak.Figure(size = (650,650))
+fig = mak.Figure(size = (800,400))
 ax = mak.Axis(fig[1,1], xgridvisible=false, ygridvisible=false,)
 
 colors_atm = mak.cgrad(:Blues_3, ND-1) 
+colors_aba = mak.cgrad(:Greens_3, ND-2) 
 colors_cor = mak.cgrad(:BuPu_3, NC) 
 
 for r in radius
-    mak.arc!(ax, 0.0, r, 0, π/2, color=:gray, linewidth=0.5,alpha=0.5)
+    mak.arc!(ax, 0.0, r, 0, π, color=:gray, linewidth=0.5,alpha=0.5)
 end
 
 for p in imppar_atm
-    mak.lines!(ax,[0.0,r_max],[p,p], linewidth=2, alpha=0.2, color=:black)
+    mak.lines!(ax,[-r_max,r_max],[p,p], linewidth=2, alpha=0.2, color=:black)
 end
 
 for p in imppar_cor
@@ -71,6 +84,12 @@ for p in collect(1:ND-1)
     imppar = fill(imppar_atm[p], length(zrays_atm[p]))
     zaxis = zrays_atm[p]
     mak.scatter!(ax, zaxis, imppar[end:-1:1], color=colors_atm[p])
+end
+
+for p in collect(1:ND-1)
+    imppar = fill(imppar_atm[p], length(zrays_aba[p]))
+    zaxis = zrays_aba[p]
+    mak.scatter!(ax, zaxis, imppar[end:-1:1], color=colors_aba[p])
 end
 
 for p in collect(1:NC)
