95fa21aec6dd494c8bf638a36207fee1f4169e9f
matheus-bp <math.bp.sci@proton.me>
2026-06-30 16:21:53 +0200

    created file to compute quadrature of FRT

---
+++ TestLibs/OK87-quadrature.jl
@@ -0,0 +1,7 @@
+# Module designed to perform the numerical intergration (quadrature) of the Formal Solution of the Radiative Transfer according to Oslon & Kunasz (1987). The implemetation is also based on the lecture notes of ?????
+
+
+ND = 20
+
+cells = collect(1:1:ND-1)
+walls = collect(1:1:ND) 
