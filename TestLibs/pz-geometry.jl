1ab29639ba7e96346a8a88e05ca382067da9808b
matheus-bp <math.bp.sci@proton.me>
2026-06-30 15:30:46 +0200

    dont remember

--- TestLibs/pz-geometry.jl
+++ TestLibs/pz-geometry.jl
@@ -1,3 +1,11 @@
+#######################
+if abspath(PROGRAM_FILE) == @__FILE__
+
+Pkg.add("CairoMakie")
+using Pkg
+using CairoMakie
+const mak = CairoMakie  
+
 # create a grid with the pz geometry scheme
 
 ND :: Int64 = 50 # number of depthpoints, i.e., the number of atmospheric impact parameters
@@ -18,7 +26,7 @@ imppar_atm = radius[1:end-1]
 imppar_cor = collect(range(r_min, 0, length=NC)) # assumes a linear spacing of the core rays
 
 zrays_atm = [Vector{Float64}() for _ in 1:ND-1]
-zrays_aba =  [Vector{Float64}() for _ in 1:ND-1]
+zrays_aba =  [Vector{Float64}() for _ in 1:ND-1] # aba := Atmosphere BAck
 zrays_cor = [Vector{Float64}() for _ in 1:NC]
 
 for (j,p) in enumerate(imppar_atm)
@@ -46,18 +54,6 @@ for (j,p) in enumerate(imppar_cor)
     end
 end
 
-########################
-
-if abspath(PROGRAM_FILE) == @__FILE__
-
-try
-    using CairoMakie
-catch
-    using Pkg
-    Pkg.add("CairoMakie")
-    using CairoMakie
-end
-const mak = CairoMakie  
     
 fig = mak.Figure(size = (800,400))
 ax = mak.Axis(fig[1,1], xgridvisible=false, ygridvisible=false,)
@@ -99,13 +95,65 @@ for p in collect(1:NC)
 end
 
 
-
-
 display(fig)
 println("Press Enter to close...")
 readline()  # Wait for user to press Enter
 
 ########################
 else
-    println("imported pz-geometry.jl")
+########################
+
+"""
+    pz_geometry_rays(radius, NC)
+
+TBW
+"""
+function pz_geometry_rays(radius, NC)
+
+# impact parameters 
+imppar_atm = radius[1:end-1]
+imppar_cor = collect(range(r_min, 0, length=NC)) # assumes a linear spacing of the core rays
+
+zrays_atm = [Vector{Float64}() for _ in 1:ND-1]
+zrays_aba =  [Vector{Float64}() for _ in 1:ND-1] # aba := Atmosphere BAck
+zrays_cor = [Vector{Float64}() for _ in 1:NC]
+
+for (j,p) in enumerate(imppar_atm)
+    for i in collect(Int64, 1:j)
+        z = (radius[i]^2 - p^2)^0.5
+        push!(zrays_atm[j], z)
+    end
+end
+
+for (j,p) in enumerate(imppar_atm)
+    for i in collect(Int64, 1:j)
+        z = -(radius[i]^2 - p^2)^0.5
+        push!(zrays_aba[j], z)
+    end
+end
+for z in zrays_aba
+    pop!(z)
+end
+
+for (j,p) in enumerate(imppar_cor)
+    for (i,r) in enumerate(radius[end:-1:1])
+        z = (r^2 - p^2)^0.5
+        push!(zrays_cor[j], z)
+    end
+end
+
+return imppar_atm, imppar_cor, zrays_atm, zrays_aba, zrays_cor
+
+end
+
+
+
+
+
+
+
+
+
+
+
 end
